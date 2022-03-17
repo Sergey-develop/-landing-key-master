2d7875db72a716526fcff99a110f9665841c2a72
Sergey-develop <84603174+Sergey-develop@users.noreply.github.com>
2022-03-17 12:51:26 +0300

    create landing

---
+++ Schluesseldienst-hamburg-tag-und-nacht.de/scripts/script.js
@@ -0,0 +1,87 @@
+$(document).on('ready', function () {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: false,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function () {
+    $('.accordion-tabs').children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+    $('.accordion-tabs').on('click', 'li > a', function (event) {
+        if (!$(this).hasClass('is-active')) {
+            event.preventDefault();
+            $('.accordion-tabs .is-open').removeClass('is-open').hide();
+            $(this).next().toggleClass('is-open').toggle();
+            $('.accordion-tabs').find('.is-active').removeClass('is-active');
+            $(this).addClass('is-active');
+        } else {
+            event.preventDefault();
+        }
+    });
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+function myMap() {
+    var mapOptions = {
+        center: new google.maps.LatLng(51.5, -0.12),
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    }
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+
+myMap();
+
